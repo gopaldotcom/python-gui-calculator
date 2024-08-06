add038d2e9ca45fc326e00f855b2a11438403531
gopaldotcom <gopaltomar008@gmail.com>
2024-08-06 18:04:38 +0530

    Update calc-gui.py

--- calc-gui.py
+++ calc-gui.py
@@ -206,3 +206,56 @@ def equals():
             recent_label_1.config(text=Last_Display)
             
             
+         
+            
+
+window = Tk()
+window.config(padx=15, pady=15, bg=COLORS[0], highlightthickness=0)
+window.title("Python calc")
+
+
+display_frame = Frame(window)
+display_frame.config(bg=COLORS[0], pady=15, highlightthickness=0)
+display_frame.grid(column=0, row=0)
+recent_label_2 = Label(display_frame, text="", bg=COLORS[0], fg=COLORS[3])
+recent_label_2.grid(column=0, row=1, sticky='e')
+recent_label_1 = Label(display_frame, text="", bg=COLORS[0], fg=COLORS[3])
+recent_label_1.grid(column=0, row=2, sticky="e")
+
+
+display_entry = Entry(display_frame, text="Display", justify=RIGHT, width=24, highlightthickness=0, font=DISPLAY_FONT, bg=COLORS[2], fg=COLORS[0])
+
+display_entry.grid(column=0, row=3)
+
+
+button_frame = Frame(window)
+button_frame.grid(column=0,row=2)
+     
+
+clear_button = Button(button_frame, text="C", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=clear, bg=COLORS[1], fg=COLORS[3])
+clear_button.grid(column=0, row=0)
+parentheses_button = Button(button_frame, text="( )", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=parentheses, bg=COLORS[2], fg=COLORS[3])
+parentheses_button.grid(column=1, row=0)
+percent_button = Button(button_frame, text="%", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("%"), bg=COLORS[2], fg=COLORS[3])
+percent_button.grid(column=2, row=0)
+division_button = Button(button_frame, text="/", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("/"), bg=COLORS[2], fg=COLORS[3])
+division_button.grid(column=3, row=0)
+
+seven_button = Button(button_frame, text="7", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("7"), bg=COLORS[3], fg=COLORS[0])
+seven_button.grid(column=0, row=1)
+eight_button = Button(button_frame, text="8", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("8"), bg=COLORS[3], fg=COLORS[0])
+eight_button.grid(column=1, row=1)
+nine_button = Button(button_frame, text="9", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("9"), bg=COLORS[3], fg=COLORS[0])
+nine_button.grid(column=2, row=1)
+multiplication_button = Button(button_frame, text="*", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("*"), bg=COLORS[2], fg=COLORS[3])
+multiplication_button.grid(column=3, row=1)
+
+
+four_button = Button(button_frame, text="4", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("4"), bg=COLORS[3], fg=COLORS[0])
+four_button.grid(column=0, row=2)
+five_button = Button(button_frame, text="5", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("5"), bg=COLORS[3], fg=COLORS[0])
+five_button.grid(column=1, row=2)
+six_button = Button(button_frame, text="6", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:number_input("6"), bg=COLORS[3], fg=COLORS[0])
+six_button.grid(column=2, row=2)
+subtraction_button = Button(button_frame, text="-", font=BUTTON_FONT, width=3,height=1, highlightthickness=0, command=lambda:operator_button("-"), bg=COLORS[2], fg=COLORS[3])
+subtraction_button.grid(column=3, row=2)
